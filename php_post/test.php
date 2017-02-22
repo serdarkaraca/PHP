67cbf96eaef153e2c182383fac93573d08cb5477
Serdar KARACA <serdarkaraca13@gmail.com>
2017-02-22 14:16:36 +0300

    PHP ile POST Fonksiyonu
    PHP ile post fonksiyonu kullanımı

---
+++ php_post/test.php
@@ -0,0 +1,12 @@
+<?php
+$isim = $_POST["name"];
+$soyisim = $_POST["surname"];
+
+/*
+$_GET methodundan gelen verileri $isim ve $soyisim değişkenlerine atadık.
+*/
+
+
+echo 'Adınız : ' .$isim . "<br>"; 
+echo 'Soyadınız : ' .$soyisim . "<br>";
+?>
