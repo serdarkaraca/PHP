6ca5fdc4c1f12fb07510ac4ff7adbdba04c691c2
Serdar KARACA <serdarkaraca13@gmail.com>
2017-02-22 14:25:18 +0300

    PHP Get Fonksiyonu
    PHP ile GET Fonksiyonu Kullanımı

---
+++ php_get/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>PHP $_GET Method Demo</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+
+<form action="test.php" method="get">
+
+İsmi: <input name="name" type="text" />
+<br>
+Soyisim: <input name="surname" type="text" />
+
+<input type="submit"  value="Gönder"/>
+</form>
+
+</body>
+</html>
---
+++ php_get/test.php
@@ -0,0 +1,12 @@
+<?php
+$isim = $_GET["name"];
+$soyisim = $_GET["surname"];
+
+/*
+$_GET methodundan gelen verileri $isim ve $soyisim değişkenlerine atadık.
+*/
+
+
+echo 'Adınız : ' .$isim . "<br>"; 
+echo 'Soyadınız : ' .$soyisim . "<br>";
+?>
